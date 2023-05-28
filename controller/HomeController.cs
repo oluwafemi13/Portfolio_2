a07eb48a16e37f8fa718750434c6b5c07a45cac2
IKECHUKWU <sundayjabikem@gmail.com>
2023-05-28 17:38:44 +0100

    Add project files.

---
+++ controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio2.controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
